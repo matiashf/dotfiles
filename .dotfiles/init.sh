cf1bf72e6b506251004c51eadcc3963e88ede70c
Matias Hermanrud Fjeld <matiashf@gmail.com>
2020-07-19 00:54:43 +0200

    Install oh-my-zsh

--- .dotfiles/init.sh
+++ .dotfiles/init.sh
@@ -7,3 +7,7 @@ cd "$(dirname "$0")"
 # Install ubuntu packages
 (cat packages.txt | xargs dpkg -s > /dev/null) \
   || (cat packages | xargs sudo apt install -y)
+
+# Install oh my zsh and set shell to zsh
+[ "$SHELL" = /usr/bin/zsh -a -e ~/.oh-my-zsh ] \
+  || sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
