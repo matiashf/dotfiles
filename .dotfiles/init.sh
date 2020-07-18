05046977d4e45603ac216151269450a9540c1b97
Matias Hermanrud Fjeld <matiashf@gmail.com>
2020-07-19 01:05:36 +0200

    Fixed typo

--- .dotfiles/init.sh
+++ .dotfiles/init.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 
 # Install ubuntu packages
 (cat packages.txt | xargs dpkg -s > /dev/null) \
-  || (cat packages | xargs sudo apt install -y)
+  || (cat packages.txt | xargs sudo apt install -y)
 
 # Install oh my zsh and set shell to zsh
 [ "$SHELL" = /usr/bin/zsh -a -e ~/.oh-my-zsh ] \
