deff394a1c0f66f6921f120a645435a03c40457b
Matias Hermanrud Fjeld <matias.hermanrud.fjeld@sgfinans.no>
2020-07-22 20:27:26 +0200

    Add alias for dotfiles command

---
+++ .zshrc.d/dotfiles.zsh
@@ -0,0 +1,3 @@
+# Idea from https://harfangk.github.io/2016/09/18/manage-dotfiles-with-a-git-bare-repository.html
+
+alias dotfiles='/usr/bin/git --git-dir="$HOME/.dotfiles.git/" --work-tree="$HOME"'
